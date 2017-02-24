c36256582b223c6359924c9847fe8b79125f5744
Davi Teodoro <davi.a.teodoro@gmail.com>
2017-02-24 19:44:43 -0300

    add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -105,7 +105,6 @@ app.post('/users', (req, res) => {
 });
 
 // POST /users/login {email , password}
-
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email'], ['password']);
 
@@ -116,7 +115,6 @@ app.post('/users/login', (req, res) => {
     }).catch((e) => res.status(400).send(e));
 
 });
-
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
